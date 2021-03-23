835d394deeb1b8c984098f7e3a1ef6816a5dd3b9
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-23 14:28:09 +0100

    Add the relation for attendence and scope for past and upcoming

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
+  has_many :attendances
+  scope :past, -> { where('date < ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
 end
