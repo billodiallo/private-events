1dcd25f5dd95fe075183f6b5aca417467f1ef60f
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-23 10:52:54 +0100

    Add navbar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -64,6 +64,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:name, :description, :location, :date, :user_id)
+      params.require(:event).permit(:name, :description, :location, :date)
     end
 end
