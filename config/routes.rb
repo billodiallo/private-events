52960d7e57ea316c8f3c0e1c3ed0f1291d624167
Billo Diallo <billodiallonet@gmail.com>
2021-03-23 15:55:06 +0300

    Add attendance model and migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :attendances
-  resources :participants
   resources :events
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
