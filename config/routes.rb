2f89a3a8d686430a070be33936962c1315ed850d
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-23 14:29:41 +0100

    Remove destroy and edit on show and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :attendances
-  resources :events
   devise_for :users
+  resources :events, only: %i[new show create index]
+  resources :users, only: %i[new show create index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "events#index"
 end
