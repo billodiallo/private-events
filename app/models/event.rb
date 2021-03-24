d9a00ae40925a768bd5ecda8f2bcfaed50f71276
Billo Diallo <billodiallonet@gmail.com>
2021-03-24 15:39:51 +0300

    Add association

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,5 @@ class Event < ApplicationRecord
   belongs_to :user
   scope :past, -> { where('date < ?', Date.today) }
   scope :upcoming, -> { where('date >= ?', Date.today) }
+  has_many :visitors, class_name: 'User', foreign_key:'user_id'
 end
