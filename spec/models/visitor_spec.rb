56f1bb133c55c98cc9034cb60688ed2b0829fa6b
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-25 15:02:10 +0100

    Add rspec for visitor inside spec

--- spec/models/visitor_spec.rb
+++ spec/models/visitor_spec.rb
@@ -2,19 +2,20 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe Visitor, type: :model do
-    it 'validations' do  
-    expect{should validate_presence_of(attendee_id)}
-    end
-  
+  it 'validations' do
+    expect { should validate_presence_of(attendee_id) }
   end
-  describe 'ActiveRecord associations' do
-    it 'has many created_events' do
-      expect{should has_many(created_events)}
-    end
-    context 'Associations' do
-        it { should belong_to(:attendee).class_name('User') }
-        it { should belong_to(:attended_event).class_name('Event') }
-      end
+end
+describe 'ActiveRecord associations' do
+  it 'has many created_events' do
+    expect { should has_many(created_events) }
   end
-
-
+  context 'Associations' do
+    it 'belongs to attended event' do
+      expect { should belongs_to(:attendee).class_name('User').foreign_key('user_id') }
+    end
+    it 'belongs_to attendee' do
+      expect { should belongs_to(:attended_event).class_name('Event').foreign_key('event_id') }
+    end
+  end
+end
