832f0d6261df730660e13faba831f962907fe0b4
Billo Diallo <billodiallonet@gmail.com>
2021-03-25 11:11:57 +0300

    Add foreign key

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,6 +1,4 @@
 class Visitor < ApplicationRecord
-  # belongs_to :user
-  # belongs_to :event
-  belongs_to :attendee, class_name: 'User'
-  belongs_to :attended_event, class_name: 'Event'
+  belongs_to :attended_event, class_name: 'Event', foreign_key: 'event_id'
+  belongs_to :attendee, class_name: 'User', foreign_key: 'user_id'
 end
