77dbdd7330e05fba68ddcd9983e65e750a74eed7
Billo Diallo <billodiallonet@gmail.com>
2021-03-25 11:10:44 +0300

    Add validates,foreign key and scope

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,15 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key:'user_id'
+  has_many :visitors, dependent: :destroy
+  has_many :attendees, through: :visitors, class_name: 'User', foreign_key: :user_id
+
   scope :past, -> { where('date < ?', Date.today) }
   scope :upcoming, -> { where('date >= ?', Date.today) }
-  has_many :visitors, class_name: 'User', foreign_key:'user_id'
+
+  validates :user_id, :title, :description, :date, :location, presence: true, length: { maximum: 50 }
+
+#   belongs_to :user
+#   scope :past, -> { where('date < ?', Date.today) }
+#   scope :upcoming, -> { where('date >= ?', Date.today) }
+  # has_many :visitors, class_name: 'User', foreign_key:'user_id'
 end
