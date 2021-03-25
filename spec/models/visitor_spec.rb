0c9a20623906ffc10997c52c8737034fd13eafff
Billo Diallo <billodiallonet@gmail.com>
2021-03-25 16:53:04 +0300

    Fix rubocop errors

--- spec/models/visitor_spec.rb
+++ spec/models/visitor_spec.rb
@@ -2,19 +2,16 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe Visitor, type: :model do
-    it 'validations' do  
-    expect{should validate_presence_of(attendee_id)}
-    end
-  
+  it 'validations' do
+    expect { should validate_presence_of(attendee_id) }
   end
-  describe 'ActiveRecord associations' do
-    it 'has many created_events' do
-      expect{should has_many(created_events)}
-    end
-    context 'Associations' do
-        it { should belong_to(:attendee).class_name('User') }
-        it { should belong_to(:attended_event).class_name('Event') }
-      end
+end
+describe 'ActiveRecord associations' do
+  it 'has many created_events' do
+    expect { should has_many(created_events) }
   end
-
-
+  context 'Associations' do
+    it { should belong_to(:attendee).class_name('User') }
+    it { should belong_to(:attended_event).class_name('Event') }
+  end
+end
