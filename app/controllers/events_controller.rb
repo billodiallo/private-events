6ed91626bd7229cb7e70397c188d7536198ccfa5
Billo Diallo <billodiallonet@gmail.com>
2021-03-25 11:02:59 +0300

    Add visitor and cancel_visitor and Update private method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,69 +1,133 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :set_event, only: %i[show edit update destroy]
+
   before_action :authenticate_user!, except: [:index]
+
   # GET /events or /events.json
+
   def index
     @events = Event.all
   end
 
   # GET /events/1 or /events/1.json
-  def show
-  end
+
+  def show; end
 
   # GET /events/new
+
   def new
     @event = Event.new
   end
 
   # GET /events/1/edit
-  def edit
-  end
+
+  def edit; end
 
   # POST /events or /events.json
+
   def create
-    @event = current_user.events.build(event_params)
+    @event = current_user.created_events.build(event_params)
 
     respond_to do |format|
       if @event.save
-        format.html { redirect_to @event, notice: "Event was successfully created." }
+
+        @event.attendees << current_user
+
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+
         format.json { render :show, status: :created, location: @event }
+
       else
+
         format.html { render :new, status: :unprocessable_entity }
+
         format.json { render json: @event.errors, status: :unprocessable_entity }
+
       end
     end
   end
 
   # PATCH/PUT /events/1 or /events/1.json
+
   def update
     respond_to do |format|
       if @event.update(event_params)
-        format.html { redirect_to @event, notice: "Event was successfully updated." }
+
+        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+
         format.json { render :show, status: :ok, location: @event }
+
       else
+
         format.html { render :edit, status: :unprocessable_entity }
+
         format.json { render json: @event.errors, status: :unprocessable_entity }
+
       end
     end
   end
 
   # DELETE /events/1 or /events/1.json
+
   def destroy
     @event.destroy
+
     respond_to do |format|
-      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+
       format.json { head :no_content }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
+  def display_join(current_user, event)
+    if current_user.attended_events.include? event
 
-    # Only allow a list of trusted parameters through.
-    def event_params
-      params.require(:event).permit(:title, :description, :location, :date, :user_id)
+      content_tag(:td, (link_to 'Unjoin', destroy_path(event_id: event.id, attendee_id: current_user.id)))
+
+    else
+
+      content_tag(:td, (link_to 'Join', join_path(event_id: event.id)))
+
     end
+  end
+
+  def creator_destroy(current_user, event)
+    return unless current_user == event.creator
+
+    content_tag(:td, (link_to 'Destroy', event, method: :delete, data: { confirm: 'Are you sure?' }))
+  end
+
+  def visitor
+    @event = Event.find(params[:id])
+    if @event.attendees.include?(current_user)
+      redirect_to @event, notice: 'You are already on the list'
+    else
+      @event.attendees << current_user
+      redirect_to @event
+    end
+  end
+
+  def cancel_visitor
+    @event = Event.find(params[:id])
+    @event.attendees.delete(current_user)
+    redirect_to @event, notice: 'You are no longer attending this event'
+  end
+
+  def operation_name(event)
+    event.id.nil? ? 'Create Event' : 'Update Event'
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+
+  def event_params
+    params.require(:event).permit(:creator_id, :title, :description, :location, :date, :user_id)
+  end
 end
