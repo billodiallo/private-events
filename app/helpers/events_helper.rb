0a5a26018027ff524cbb4372a34f5cd317ec7809
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-25 10:19:10 +0100

    Add bootstrap to the navbar

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,12 +2,12 @@ module EventsHelper
   def user_sessions
     if user_signed_in?
       content_tag(:div, class: 'd-flex special') do
-        (button_to 'sign out', destroy_user_session_path, method: :delete, class: 'btn_all bg-primary') +
-          (link_to 'new post', new_event_path, method: :delete, class: 'btn_all text-light btn_special1')
+        (button_to 'Sign Out', destroy_user_session_path, method: :delete, class: 'btn_all bg-primary') +
+          (link_to 'New Event', new_event_path, method: :delete, class: 'btn_all text-light btn_special1')
       end
     else
-      (button_to 'sign in', new_user_session_path, class: 'btn_all bg-primary') +
-        (link_to 'sign up', new_user_registration_path, class: 'btn_special1 text-light btn_all')
+      (button_to 'Sign In', new_user_session_path, class: 'btn_all bg-primary') +
+        (link_to 'Sign Up', new_user_registration_path, class: 'btn_special1 text-light btn_all')
     end
   end
 
