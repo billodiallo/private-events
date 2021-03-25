149060a4c5c8ea7355120bf4fbc3d5c984f6173a
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-25 13:45:15 +0100

    Add spec for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,23 @@ RSpec.describe User, type: :model do
     expect(user.valid?).to be(false)
   end
   it 'validates  email and password' do
-    user = User.new( email: 'andrei@gmail.com' ,password:'123456')
-    expect(user.valid?).to be(true)
+    user1 = User.new( email: 'andrei@gmail.com' ,password:'123456')
+    expect(user1.valid?).to be(true)
+  end
+  it 'return false if user password is less the 6' do
+    user2 = User.new(email: 'billo@gmail.com', password: '12345')
+    expect(user2.valid?).to be(false)
+  end
+  describe 'ActiveRecord associations' do
+    it 'has many created_events' do
+      expect{should has_many(created_events)}
+    end
+    it 'has many visitors' do
+      expect{should has_many(visitors).with_foreign_key}
+    end
+    it 'has many attended_events' do
+      expect{should has_many(attended_events).through(visitors)}
+    end
   end
 end
+
