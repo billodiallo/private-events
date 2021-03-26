4c9f79c53c1a9b8d5109984a79e5bb27cfbc4c4e
Denis Diaconu <denisdiaconu117@yahoo.com>
2021-03-26 10:43:32 +0100

    Add method inside show

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -10,4 +10,35 @@ module EventsHelper
         (link_to 'Sign Up', new_user_registration_path, class: 'btn_special1 text-light btn_all')
     end
   end
+
+  def event_option
+    if current_user == @event.creator
+      (link_to 'Edit', edit_event_path, class: 'btn btn-outline-secondary')
+      (link_to 'Delete', event_path, method: :delete, class: 'btn btn-outline-secondary',
+                                     data: { confirm: 'Delete this event?' })
+    elsif @event.attendees.include?(current_user)
+      (link_to 'Cancel visitor', cancel_visitor_event_path, class: 'btn btn-outline-secondary')
+    else
+      (link_to 'Want to Join --- Go ', visitor_event_path, class: 'btn btn-primary' unless @event.date < Time.now)
+    end
+  end
+
+  def event_option2
+    if @event.attendees.empty?
+      content_tag(:div, class: 'list-group') do
+        content_tag(:li, class: 'list-group-item text-muted') do
+          text = 'This event has no attendees yet'
+          text
+        end
+      end
+    else
+      content_tag(:div, class: 'list-group') do
+        @event.attendees.each do |attendee|
+          content_tag(:li, class: 'list-group-item') do
+            attendee.email
+          end
+        end
+      end
+    end
+  end
 end
